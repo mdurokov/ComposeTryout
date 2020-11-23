875d6ae9964531c4be56b50fb8e0d5be8351d77f
mdurokov <matej@tenzor.tech>
2020-11-23 15:02:12 +0100

    complex custom layout done (staggered grid)

--- ComposeLayout/SlotApis/app/src/main/java/com/example/materialcomponents/MainActivity.kt
+++ ComposeLayout/SlotApis/app/src/main/java/com/example/materialcomponents/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.materialcomponents
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ScrollableRow
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -22,7 +25,15 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.materialcomponents.ui.MaterialComponentsTheme
+import kotlin.math.absoluteValue
+import kotlin.math.max
 
+
+val topics = listOf(
+        "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+        "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+        "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,13 +69,85 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier){
-    MyOwnColumn(modifier.padding(8.dp)) {
-        Text("MyOwnColumn")
-        Text("places items")
-        Text("verticaly")
-        Text("We've done it by hand!")
+    ScrollableRow(modifier = modifier) {
+        StaggeredGrid(modifier = modifier) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
     }
 }
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    children: @Composable() () -> Unit
+){
+    Layout(modifier = modifier, children = children){ measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowMaxHeights = IntArray(rows) { 0 }
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+            val row = index % rows
+            rowWidths[row] = rowWidths[row] + placeable.width.absoluteValue
+            rowMaxHeights[row] = max(rowMaxHeights[row], placeable.height.absoluteValue)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+                ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowMaxHeights.sumBy { it }
+                .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+        val rowY = IntArray(rows) { 0 }
+
+        for(i in 1 until rows){
+            rowY[i] = rowY[i-1] + rowMaxHeights[i-1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) {0}
+            placeables.forEachIndexed{ index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String){
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier.preferredSize(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(modifier = Modifier.preferredWidth(4.dp))
+            Text(text)
+        }
+    }
+}
+
+/*@Preview
+@Composable
+fun ChipPreview(){
+    MaterialComponentsTheme {
+        Chip(text = "Hi Codelab!")
+    }
+}*/
 
 fun Modifier.firstBaselineToTop(
     firstBaselineToTop: Dp
